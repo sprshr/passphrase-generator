49e8013d63e530121b41ef51728d468a79f5b52d
Seyed Sepehr Sahraian <68083280+sprshr@users.noreply.github.com>
2021-11-30 03:03:33 +0000

    add generate function

--- passphrase-generator.py
+++ passphrase-generator.py
@@ -1,4 +1,6 @@
 import random
+#global var
+again = "y"
 symbols = ("`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=",
            "+", "[", "{", "]", "}", ";", ":", "'", '"', ",", "<", ".", ">", "/", "?", "|", "")
 letters = ("asdfghjklzxcvbnmqwertyuiop")
@@ -7,34 +9,63 @@ print(len(letters))
 print("Passphrase Generator")
 print("By Sepehr Sahraian")
 print("AP CSP Class")
+#generator functions
 
-
-def selectLetter(case):
+def addUpperCase():
     letterSelected = letters[random.randint(0, 25)]
-    if case == "upper":
-        return letterSelected.upper()
-    else:
-        return letterSelected
+    return letterSelected.upper()
 
+def addLowerCase():
+    letterSelected = letters[random.randint(0, 25)]
+    return letterSelected
+
+def addNumber():
+    return str(random.randint(0, 9))
+
+def addSymbol():
+    return random.choice(symbols)
+
+def generate(length):
+    passphrase = ""
+    if includeLowerCase == "n":
+        functions.pop(0)
+    if includeUpperCase == "n":
+        functions.pop(1)
+    for n in range(length):
+        generated = random.choice(functions)()
+        passphrase = passphrase + generated
+    return passphrase
+        
+
+    
 
 # generator loop
 while True:
     functions = []
-
+    passLength = 0
     # lower case
+    print("Enter the passphrase Length")
+    passLength = int(input())
+    while passLength <= 0:
+        print("Please enter a valid number")
+        passLength == input()
     print("Include Lower Case? (y/n)")
     includeLowerCase = input()
     while includeLowerCase != "y" and includeLowerCase != "n":
         print("please enter y for yes and n for no")
         print("Include Lower Case? (y/n)")
         includeLowerCase = input()
+        if includeLowerCase == "y":
+            functions.append(addLowerCase)
     # upper case
     print("Include Upper Case? (y/n)")
-    includeupperCase = input()
-    while includeupperCase != "y" and includeupperCase != "n":
+    includeUpperCase = input()
+    while includeUpperCase != "y" and includeUpperCase != "n":
         print("please enter y for yes and n for no")
         print("Include Upper Case? (y/n)")
         includeupperCase = input()
+        if includeUpperCase == "y":
+            functions.append(addUpperCase)
     # symbols
     print("Include Symbols? (y/n)")
     includeSymbol = input()
@@ -42,6 +73,8 @@ while True:
         print("please enter y for yes and n for no")
         print("Include Symbols? (y/n)")
         includeSymbol = input()
+    if includeSymbol == "y":
+        functions.append(addSymbol)
     # numbers
     print("Include Numbers? (y/n)")
     includeNumber = input()
@@ -49,3 +82,12 @@ while True:
         print("please enter y for yes and n for no")
         print("Include Numbers? (y/n)")
         includeNumber = input()
+    if includeNumber == "y":
+        functions.append(addNumber)
+    while again == "y":
+        print(generate(passLength))
+        print("Generate a New Passphrase?(y/n)")
+        again == input()
+        if input != "y" and input != "n":
+            print("Please Enter y for Yes and n for No")
+            again == input()
