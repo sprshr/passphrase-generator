48c0ef5af249e82e2b54bc8fc9eb0cbcab0ecdf5
Sepehr Sahraian <sep3831@gmail.com>
2021-12-11 13:37:56 -0800

    change documentation

--- passphrase-generator.py
+++ passphrase-generator.py
@@ -8,37 +8,9 @@ letters = ("asdfghjklzxcvbnmqwertyuiop")
 print("Passphrase Generator")
 print("By Sepehr Sahraian")
 print("AP CSP Class")
-# generator functions
 
 
-def addUpperCase():
-    letterSelected = letters[random.randint(0, 25)]
-    return letterSelected.upper()
-
-
-def addLowerCase():
-    letterSelected = letters[random.randint(0, 25)]
-    return letterSelected
-
-
-def addNumber():
-    return str(random.randint(0, 9))
-
-
-def addSymbol():
-    return random.choice(symbols)
-
-
-def generate(length):
-    passphrase = ""
-    if length > 1000000:
-        print("It might take longer than usual, please wait")
-    for n in range(length):
-        generated = random.choice(functions)()
-        passphrase = passphrase + generated
-    return passphrase
-
-
+# This function asks for the length of the passphrase
 def askLength():
     global passLength
     print("Enter the passphrase Length")
@@ -53,34 +25,7 @@ def askLength():
         passLength = input()
 
 
-def incNumber():
-    # numbers
-    print("Include Numbers? (y/n)")
-    includeNumber = input()
-    while includeNumber != "y" and includeNumber != "n":
-        print("please enter y for yes and n for no")
-        print("Include Numbers? (y/n)")
-        includeNumber = input()
-    if includeNumber == "y":
-        functions.append(addNumber)
-        global noCharacter
-        noCharacter = False
-
-
-def incUpper():
-    # upper case
-    print("Include Upper Case? (y/n)")
-    includeUpperCase = input()
-    while includeUpperCase != "y" and includeUpperCase != "n":
-        print("please enter y for yes and n for no")
-        print("Include Upper Case? (y/n)")
-        includeupperCase = input()
-    if includeUpperCase == "y":
-        functions.append(addUpperCase)
-        global noCharacter
-        noCharacter = False
-
-
+# Whehter or not include lower case letters
 def incLower():
     print("Include Lower Case? (y/n)")
     includeLowerCase = input()
@@ -94,6 +39,35 @@ def incLower():
         noCharacter = False
 
 
+# Whehter or not include upper case letters
+def incUpper():
+    print("Include Upper Case? (y/n)")
+    includeUpperCase = input()
+    while includeUpperCase != "y" and includeUpperCase != "n":
+        print("please enter y for yes and n for no")
+        print("Include Upper Case? (y/n)")
+        includeupperCase = input()
+    if includeUpperCase == "y":
+        functions.append(addUpperCase)
+        global noCharacter
+        noCharacter = False
+
+
+# Whehter or not include numbers
+def incNumber():
+    print("Include Numbers? (y/n)")
+    includeNumber = input()
+    while includeNumber != "y" and includeNumber != "n":
+        print("please enter y for yes and n for no")
+        print("Include Numbers? (y/n)")
+        includeNumber = input()
+    if includeNumber == "y":
+        functions.append(addNumber)
+        global noCharacter
+        noCharacter = False
+
+
+# Whehter or not include symbols
 def incSymbol():
     print("Include Symbols? (y/n)")
     includeSymbol = input()
@@ -105,6 +79,41 @@ def incSymbol():
         functions.append(addSymbol)
         global noCharacter
         noCharacter = False
+
+
+# generator functions
+
+# This function returns a random upper case letter
+def addUpperCase():
+    letterSelected = letters[random.randint(0, 25)]
+    return letterSelected.upper()
+
+
+# This function returns a random lower case letter
+def addLowerCase():
+    letterSelected = letters[random.randint(0, 25)]
+    return letterSelected
+
+
+# This function returns a random one digit number
+def addNumber():
+    return str(random.randint(0, 9))
+
+
+# This function returns a random symbol
+def addSymbol():
+    return random.choice(symbols)
+
+
+# This function is exectued based on Length and combines all the returned value(s) from function above
+def generate(length):
+    passphrase = ""
+    if length > 1000000:
+        print("It might take longer than usual, please wait")
+    for n in range(length):
+        generated = random.choice(functions)()
+        passphrase = passphrase + generated
+    return passphrase
 
 
 # generator loop
